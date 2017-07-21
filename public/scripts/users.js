62b3da6c126fd22ac70f3dd58d7ea600ca593ce5
kellybarber <kelly.james.barber@gmail.com>
2017-07-21 19:34:12 +0000

    working on delete button

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -9,10 +9,13 @@ $(document).ready((list) => {
 
     for (item in list) {
       $("<div class='item-box'>").text(list[item].title).appendTo('.user-box')
+      $("<button class='delete'>").text('delete').appendTo('.user-box')
     }
 
+    // <form method="POST" action="/urls//delete"><input type="submit" value="Delete"></form>
+
     $("<button class='update'>").text('update').appendTo('.item-box')
-    $("<button class='delete'>").text('delete').appendTo('.item-box')
+    // $("<button class='delete'>").text('delete').appendTo('.item-box')
 
     })
   })
