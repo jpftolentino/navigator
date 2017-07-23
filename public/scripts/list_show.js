afd0dbf79753e86ffc705fae02fca64b172c6201
kellybarber <kelly.james.barber@gmail.com>
2017-07-23 23:01:46 +0000

    fixed code showing list twice on list view

--- public/scripts/list_show.js
+++ public/scripts/list_show.js
@@ -26,23 +26,23 @@ $(() => {
 
       $('.list-box').append(listName).append(listCategory).append(listTime).append('<ul>')
 
-      for(item in task){
-        let taskID = task[item].task_id;
-        let listID = task[item].fk_list_id;
-        let description = task[item].description;
-        let url = task[item].url;
-        // let list
-        //
-        if(listID == id){
-
-          $('<div>').text(description).appendTo($('body'));
-          $('<div>').html('<a href=\'' + url + '\'>' + url + '</a>').appendTo($('body'));
-          $('<br/>').appendTo($('body'));
-          $('<li>').text(description).appendTo($('ul'));
-          $(`<a href="http://${url}" class="url">`).text("Go To There").appendTo($('ul'));
-
-        }
-      }
+      // for(item in task){
+      //   let taskID = task[item].task_id;
+      //   let listID = task[item].fk_list_id;
+      //   let description = task[item].description;
+      //   let url = task[item].url;
+      //   // let list
+      //   //
+      //   if(listID == id){
+      //
+      //     $('<div>').text(description).appendTo($('body'));
+      //     $('<div>').html('<a href=\'' + url + '\'>' + url + '</a>').appendTo($('body'));
+      //     $('<br/>').appendTo($('body'));
+      //     $('<li>').text(description).appendTo($('ul'));
+      //     $(`<a href="http://${url}" class="url">`).text("Go To There").appendTo($('ul'));
+      //
+      //   }
+      // }
   })
 });
 
